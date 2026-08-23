366ce8e7be30c03e6893132a6f92d3dd756c20b5
humainislam11 <humainislam467@gmail.com>
2026-08-23 23:26:10 +0600

    Fix node_modules tracking issue

--- src/assets/hooks/useAxiosSecure.jsx
+++ src/assets/hooks/useAxiosSecure.jsx
@@ -5,7 +5,7 @@ import { AuthContext } from "../../context/AuthContext";
 
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://bilal-zone-backend.onrender.com',
   });
 const useAxiosSecure = () => {
   const navigate = useNavigate();
